3f8ab5442fb14a4ca853187702e96c448939b675
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-05-17 13:37:12 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+      
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
