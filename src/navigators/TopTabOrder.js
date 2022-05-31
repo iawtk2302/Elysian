d64370bf8b586aa0563ccd621d1a4973131ede0a
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-05-31 17:05:30 +0700

    5.05 31/05/2022

--- src/navigators/TopTabOrder.js
+++ src/navigators/TopTabOrder.js
@@ -1,20 +1,24 @@
 import OnGoing from '../screens/onGoing';
 import WaiToConFirm from '../screens/waitToConfirm';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import styles from '../styles/View.TopTab.Nav';
 import CancelledOrder from '../screens/cancelledOrder';
 import CompletedOder from '../screens/completedOder';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export default TopTabOrder = () => {
   return (
-    <Tab.Navigator screenOptions={styles.screenOptions}>
-      <Tab.Screen name="Chờ xác nhận" component={WaiToConFirm} />
-      <Tab.Screen name="Đang giao" component={OnGoing} />
-      <Tab.Screen name="Hủy đơn" component={CancelledOrder} />
-      <Tab.Screen name="Hoàn thành" component={CompletedOder} />
-    </Tab.Navigator>
+    <NavigationContainer independent={true}>
+
+      <Tab.Navigator screenOptions={styles.screenOptions}>
+        <Tab.Screen name="Chờ xác nhận" component={WaiToConFirm} />
+        <Tab.Screen name="Đang giao" component={OnGoing} />
+        <Tab.Screen name="Hủy đơn" component={CancelledOrder} />
+        <Tab.Screen name="Hoàn thành" component={CompletedOder} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
